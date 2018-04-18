d3a4623adad443e254db8d6307efb779e953050f
En Shih <seanstone5923@gmail.com>
2018-04-18 23:13:05 +0800

    Add seabios package

---
+++ package/seabios/seabios.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# SeaBIOS
+#
+################################################################################
+
+SEABIOS_VERSION = rel-1.11.1
+SEABIOS_SITE = https://github.com/qemu/seabios
+SEABIOS_SITE_METHOD := git
+SEABIOS_LICENSE = LGPL-3.0
+SEABIOS_LICENSE_FILES = COPYING
+SEABIOS_DEPENDENCIES =
+SEABIOS_INSTALL_TARGET = YES
+
+define SEABIOS_BUILD_CMDS
+  $(MAKE) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" LD="$(TARGET_LD)" -C $(@D)
+endef
+
+define SEABIOS_INSTALL_TARGET_CMDS
+  $(INSTALL) -D -m 0755 $(@D)/out/bios.bin $(TARGET_DIR)/usr/share/qemu/
+endef
+
+$(eval $(generic-package))
